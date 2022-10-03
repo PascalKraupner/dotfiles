0c4e9e7b6fdb56c1b810ed25b336a453b1739a28
Pascal Kraupner <pascal.kraupner@gmail.com>
2022-10-03 13:05:31 +0200

    added plugin to neovim config

--- roles/neovim/tasks/files/lua/user/plugins.lua
+++ roles/neovim/tasks/files/lua/user/plugins.lua
@@ -51,7 +51,7 @@ return packer.startup(function(use)
   use "akinsho/bufferline.nvim"
   use "moll/vim-bbye"
   use 'nvim-lualine/lualine.nvim'
-
+  use "leafOfTree/vim-matchtag"
   -- Colorschemes
   use 'folke/tokyonight.nvim'
 
