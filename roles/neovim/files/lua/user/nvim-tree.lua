34096ff158511c5d0d2fb2550a9dc056ef287f43
Pascal Kraupner <pascal.kraupner@gmail.com>
2022-11-27 15:53:28 +0100

    updated nvim config

--- roles/neovim/files/lua/user/nvim-tree.lua
+++ roles/neovim/files/lua/user/nvim-tree.lua
@@ -1,14 +1,17 @@
 require("nvim-tree").setup({
-  view = {
-    adaptive_size = false
-  },
-  renderer = {
-    full_name = true,
-    indent_width = 1
-  },
-  actions = {
-    open_file = {
-      quit_on_open = true,
-    }
-  }
+	view = {
+		adaptive_size = false,
+	},
+	renderer = {
+		full_name = true,
+		indent_width = 1,
+	},
+	actions = {
+		open_file = {
+			quit_on_open = true,
+		},
+	},
+	update_focused_file = {
+		enable = true,
+	},
 })
