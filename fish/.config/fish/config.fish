5c861754bb81f8a5a6fd54e1588fef3db49788a5
Pascal Kraupner <pascal.kraupner@gmail.com>
2024-09-24 00:19:22 +0200

    updated configs and set up

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,11 @@
 fish_vi_key_bindings
+
+if test (uname) = "Darwin"
+    set -gx PATH /opt/homebrew/bin $PATH
+end
+
 starship init fish | source
+zoxide init --cmd cd fish | source
 
 function forward-char-and-accept-autosuggestion-if-at-end
     set -l right_of_cursor "$(string sub "$(commandline)" --start (math (commandline --cursor) + 2))"
@@ -15,4 +21,6 @@ function v
   nvim $argv
 end
 
-
+function ls
+  eza $argv
+end
--- set-up/set-up/brew.sh
+++ set-up/set-up/brew.sh
@@ -2,9 +2,14 @@
 
 ## Install script for Homebrew under MacOS
 packages=(
+"tmux"
+"nikitabobko/tap/aerospace"
 "font-jetbrains-mono-nerd-font"
+"wezterm"
 "firefox"
-"alacritty"
+"sketchybar"
+"1password"
+"hyperkey"
 "man"
 "stow"
 "neovim"
@@ -25,14 +30,24 @@ packages=(
 "spotify"
 "brave-browser"
 "localsend"
+"rectangle"
+"eza"
+"zoxide"
+"zellij"
 # NeoVIM
 "ripgrep"
 "fd"
 )
 
 brew tap localsend/localsend
+brew tap FelixKratz/formulae
+
 
 for package in "${packages[@]}"; do
 	echo "Installing $package"
-	sudo brew install "$package"
+	brew install "$package"
 done
+
+#run post install hooks
+#defaults write com.apple.dock autohide -bool true; killall Dock
+#defaults write NSGlobalDomain _HIHideMenuBar -bool true; killall SystemUIServer
