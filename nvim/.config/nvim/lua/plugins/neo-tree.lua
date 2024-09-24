a521f301a41188fad0b3c9d0959f7b5f3a2ea6ab
Pascal Kraupner <pascal.kraupner@gmail.com>
2024-09-24 23:21:21 +0200

    added neo tree instead of nvim tree

---
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,13 @@
+return {
+	"nvim-neo-tree/neo-tree.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		vim.g.loaded_netrw = 1
+		vim.g.loaded_netrwPlugin = 1
+		vim.api.nvim_set_keymap("n", "<leader>e", ":Neotree toggle<CR>", { noremap = true, silent = true })
+	end,
+}
