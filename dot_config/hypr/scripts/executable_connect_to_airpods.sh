434efb82c4d48c167411e16550e147932aebeb42
Pascal Kraupner <pascal.kraupner@gmail.com>
2025-11-04 21:29:58 +0100

    changes

--- dot_config/hypr/scripts/executable_connect_to_airpods.sh
+++ dot_config/hypr/scripts/executable_connect_to_airpods.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # The MAC address of the Bluetooth device you want to connect to
-DEVICE_MAC="74:15:F5:46:87:E6"
+DEVICE_MAC="34:0E:22:3B:33:8F"
 
 # Start bluetoothctl, then connect to the device and exit
 echo -e "connect $DEVICE_MAC\nexit" | bluetoothctl
