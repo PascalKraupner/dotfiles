cdf476a23e798f24f210cd767e2ab51ca2f05577
Pascal Kraupner <pascal@kraupner.me>
2026-02-16 16:28:38 -0500

    ignore md013 rule

--- dot_config/nvim/lua/plugins/nvim-lint.lua
+++ dot_config/nvim/lua/plugins/nvim-lint.lua
@@ -1,8 +1,15 @@
 return {
   "mfussenegger/nvim-lint",
+  optional = true,
   opts = {
     linters_by_ft = {
       php = {},
+      markdown = { "markdownlint-cli2" },
+    },
+    linters = {
+      ["markdownlint-cli2"] = {
+        args = { "--config", os.getenv("HOME") .. "/.markdownlint-cli2.yaml", "--" },
+      },
     },
   },
 }
