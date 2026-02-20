3c9c4390d5e30b9bb5a9bfba6a3592edd2f0a945
Pascal Kraupner <pascal@kraupner.me>
2026-02-20 14:43:58 -0500

    starship nvim ghostty imporovements

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,23 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Force transparent backgrounds on all UI elements
+vim.api.nvim_create_autocmd("ColorScheme", {
+  callback = function()
+    local groups = {
+      "Normal", "NormalNC", "NormalFloat",
+      "FloatBorder", "FloatTitle",
+      "SignColumn", "FoldColumn", "EndOfBuffer",
+      "TabLine", "TabLineFill", "TabLineSel",
+      "StatusLine", "StatusLineNC",
+      "WinBar", "WinBarNC",
+      "SnacksExplorer", "SnacksExplorerNormal", "SnacksExplorerWinBar",
+      "SnacksPickerList", "SnacksPickerPreview", "SnacksPickerInput",
+    }
+    for _, group in ipairs(groups) do
+      vim.api.nvim_set_hl(0, group, { bg = "none", ctermbg = "none" })
+    end
+
+  end,
+})
